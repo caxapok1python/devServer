4a3c37f4236f841dcdb1617b53fc26777a2f8e28
Danil <caxapok.js@yandex.ru>
2023-01-11 12:01:08 +0300

    fix

--- code/telegram_bot/handlers/chat/author.py
+++
@@ -1,9 +0,0 @@
-from loader import dp, bot, types
-from keyboards.author import keyboard
-from filters import isPrivate
-
-
-@dp.message_handler(isPrivate(), commands=['author'])
-async def author_command(msg: types.Message):
-    await bot.send_message(msg.chat.id, "Author Page", reply_markup=keyboard())
-
